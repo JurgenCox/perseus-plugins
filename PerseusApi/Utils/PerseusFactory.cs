7ac5225bbbc037075f00989c1041be471111f7a6
Juergen Cox <jurgen.cox@hotmail.com>
2017-04-13 18:50:36 +0200

    DLLs

--- PerseusApi/Utils/PerseusFactory.cs
+++ PerseusApi/Utils/PerseusFactory.cs
@@ -2,29 +2,74 @@
 using System.Runtime.Remoting;
 using PerseusApi.Matrix;
 
-namespace PerseusApi.Utils
-{
+namespace PerseusApi.Utils {
 	/// <summary>
 	/// Factory class that provides static methods for creating instances of data items used in Perseus
 	/// </summary>
-	public class PerseusFactory2
-	{
+	public class PerseusFactory2 {
 		/// <summary>
-		/// Creates an empty default implementation of IMatrixData
+		/// Creates an empty default implementation of <see cref="IMatrixData"/>.
 		/// </summary>
-		/// The empty MatrixData object.
-		/// <returns></returns>
-		public static IMatrixData CreateMatrixData()
-		{
-			try
-			{
+		public static IMatrixData CreateMatrixData() {
+			try {
 				ObjectHandle o = Activator.CreateInstance("MatrixData", "PerseusLibS.Data.Matrix.MatrixData");
 				return (IMatrixData) o.Unwrap();
-			}
-			catch (Exception)
-			{
-			}
+			} catch (Exception) {}
 			return null;
 		}
+
+		///// <summary>
+		///// Create minimal initialized <see cref="IMatrixData"/>.
+		///// </summary>
+		///// <param name="values"></param>
+		///// <returns></returns>
+		//public static IMatrixData CreateMatrixData(float[,] values)
+		//{
+		//	return CreateNewMatrixData(values, Enumerable.Range(1, values.GetLength(0) + 1).Select(i => $"Column {i}").ToList());
+		//}
+		///// <summary>
+		///// Create minimal initialized <see cref="IMatrixData"/>.
+		///// </summary>
+		///// <param name="values"></param>
+		///// <param name="columnNames"></param>
+		///// <returns></returns>
+		//public static IMatrixData CreateMatrixData(float[,] values, List<string> columnNames)
+		//{
+		//	IMatrixData mdata = new MatrixData();
+		//	mdata.Values.Set(values);
+		//	mdata.ColumnNames = columnNames;
+		//	var imputed = new BoolMatrixIndexer();
+		//	imputed.Init(mdata.RowCount, mdata.ColumnCount);
+		//	mdata.IsImputed = imputed;
+		//	return mdata;
+		//}
+		//public static IDocumentData CreateNewDocumentData()
+		//{
+		//	return new DocumentData();
+		//}
+		//internal static ISequenceData CreateNewSequenceData()
+		//{
+		//	return new SequenceData();
+		//}
+		//public static INetworkData CreateNewNetworkData()
+		//{
+		//	return new NetworkData();
+		//}
+		//public static IDataWithAnnotationColumns CreateDataWithAnnotationColumns()
+		//{
+		//	return new DataWithAnnotationColumns();
+		//}
+		//public static InternalData Create(DataType2 dataType)
+		//{
+		//	switch (dataType)
+		//	{
+		//		case DataType2.Matrix:
+		//			return (MatrixData)CreateNewMatrixData();
+		//		case DataType2.Network:
+		//			return (NetworkData)CreateNewNetworkData();
+		//		default:
+		//			throw new NotImplementedException($"Data type {dataType} not implemented!");
+		//	}
+		//}
 	}
 }
