adde4a14c40a110fd2c58e1e7a49773e8e675ca0
Jan Rudolph <rudolph@biochem.mpg.de>
2017-04-13 10:51:05 +0200

    unique values -- refactor, test, document

---
+++ PerseusPluginLib/PerseusPluginLib.Test/Utils/DataWithAnnotationColumnsExtensionsTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PerseusApi.Generic;
+using PerseusPluginLib.Utils;
+
+namespace PerseusPluginLib.Test.Utils
+{
+    [TestClass()]
+    public class DataWithAnnotationColumnsExtensionsTest
+    {
+        [TestMethod()]
+        public void UniqueValuesTest()
+        {
+            var moq = new Moq.Mock<IDataWithAnnotationColumns>();
+            var testList = new List<string[]> { new[] { "a;b", "a;a" } };
+            moq.Setup(data => data.StringColumns).Returns(testList);
+            var asdf = moq.Object;
+            asdf.UniqueValues(new[] { 0 });
+            CollectionAssert.AreEqual(new [] {"a;b", "a"}, testList[0]);
+        }
+    }
+}
---
+++ PerseusPluginLib/Utils/DataWithAnnotationColumnsExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using BaseLibS.Num;
+using BaseLibS.Util;
+using PerseusApi.Generic;
+
+namespace PerseusPluginLib.Utils
+{
+    public static class DataWithAnnotationColumnsExtensions
+    {
+        public static void UniqueValues(this IDataWithAnnotationColumns mdata, int[] stringCols)
+        {
+            foreach (string[] col in stringCols.Select(stringCol => mdata.StringColumns[stringCol]))
+            {
+                for (int i = 0; i < col.Length; i++)
+                {
+                    string q = col[i];
+                    if (q.Length == 0)
+                    {
+                        continue;
+                    }
+                    string[] w = q.Split(';');
+                    w = ArrayUtils.UniqueValues(w);
+                    col[i] = StringUtils.Concat(";", w);
+                }
+            }
+        }
+    }
+}
