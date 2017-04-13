c866b8084f14398bcb80f2df2da5d811e954e20b
Juergen Cox <jurgen.cox@hotmail.com>
2017-04-13 15:42:20 +0200

    Factory class for creating implementations of data objects in Perseus added

---
+++ PerseusApi/Utils/PerseusFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.Remoting;
+using PerseusApi.Matrix;
+
+namespace PerseusApi.Utils
+{
+	/// <summary>
+	/// Factory class that provides static methods for creating instances of data items used in Perseus
+	/// </summary>
+	public class PerseusFactory
+	{
+		/// <summary>
+		/// Creates an empty default implementation of IMatrixData
+		/// </summary>
+		/// The empty MatrixData object.
+		/// <returns></returns>
+		public static IMatrixData CreateMatrixData()
+		{
+			try
+			{
+				ObjectHandle o = Activator.CreateInstance("MatrixData", "PerseusLibS.Data.Matrix.MatrixData");
+				return (IMatrixData) o.Unwrap();
+			}
+			catch (Exception)
+			{
+			}
+			return null;
+		}
+	}
+}
