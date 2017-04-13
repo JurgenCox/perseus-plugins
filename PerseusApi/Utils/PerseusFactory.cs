b0fb2a41bcbf0ebbab5da88088e43581bf3ffa04
Juergen Cox <jurgen.cox@hotmail.com>
2017-04-13 21:47:17 +0200

    ´PerseusFactory methods added

--- PerseusApi/Utils/PerseusFactory.cs
+++ PerseusApi/Utils/PerseusFactory.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.Remoting;
+using BaseLibS.Num.Matrix;
+using PerseusApi.Document;
+using PerseusApi.Generic;
 using PerseusApi.Matrix;
+using PerseusApi.Network;
 
 namespace PerseusApi.Utils {
 	/// <summary>
 	/// Factory class that provides static methods for creating instances of data items used in Perseus
 	/// </summary>
-	public class PerseusFactory2 {
+	public class PerseusFactory {
 		/// <summary>
 		/// Creates an empty default implementation of <see cref="IMatrixData"/>.
 		/// </summary>
@@ -18,58 +24,55 @@ namespace PerseusApi.Utils {
 			return null;
 		}
 
-		///// <summary>
-		///// Create minimal initialized <see cref="IMatrixData"/>.
-		///// </summary>
-		///// <param name="values"></param>
-		///// <returns></returns>
-		//public static IMatrixData CreateMatrixData(float[,] values)
-		//{
-		//	return CreateNewMatrixData(values, Enumerable.Range(1, values.GetLength(0) + 1).Select(i => $"Column {i}").ToList());
-		//}
-		///// <summary>
-		///// Create minimal initialized <see cref="IMatrixData"/>.
-		///// </summary>
-		///// <param name="values"></param>
-		///// <param name="columnNames"></param>
-		///// <returns></returns>
-		//public static IMatrixData CreateMatrixData(float[,] values, List<string> columnNames)
-		//{
-		//	IMatrixData mdata = new MatrixData();
-		//	mdata.Values.Set(values);
-		//	mdata.ColumnNames = columnNames;
-		//	var imputed = new BoolMatrixIndexer();
-		//	imputed.Init(mdata.RowCount, mdata.ColumnCount);
-		//	mdata.IsImputed = imputed;
-		//	return mdata;
-		//}
-		//public static IDocumentData CreateNewDocumentData()
-		//{
-		//	return new DocumentData();
-		//}
-		//internal static ISequenceData CreateNewSequenceData()
-		//{
-		//	return new SequenceData();
-		//}
-		//public static INetworkData CreateNewNetworkData()
-		//{
-		//	return new NetworkData();
-		//}
-		//public static IDataWithAnnotationColumns CreateDataWithAnnotationColumns()
-		//{
-		//	return new DataWithAnnotationColumns();
-		//}
-		//public static InternalData Create(DataType2 dataType)
-		//{
-		//	switch (dataType)
-		//	{
-		//		case DataType2.Matrix:
-		//			return (MatrixData)CreateNewMatrixData();
-		//		case DataType2.Network:
-		//			return (NetworkData)CreateNewNetworkData();
-		//		default:
-		//			throw new NotImplementedException($"Data type {dataType} not implemented!");
-		//	}
-		//}
+		/// <summary>
+		/// Create minimally initialized <see cref="IMatrixData"/>.
+		/// </summary>
+		public static IMatrixData CreateMatrixData(float[,] values) {
+			return CreateMatrixData(values, Enumerable.Range(1, values.GetLength(0) + 1).Select(i => $"Column {i}").ToList());
+		}
+
+		/// <summary>
+		/// Create minimally initialized <see cref="IMatrixData"/>.
+		/// </summary>
+		public static IMatrixData CreateMatrixData(float[,] values, List<string> columnNames) {
+			IMatrixData mdata = CreateMatrixData();
+			mdata.Values.Set(values);
+			mdata.ColumnNames = columnNames;
+			var imputed = new BoolMatrixIndexer();
+			imputed.Init(mdata.RowCount, mdata.ColumnCount);
+			mdata.IsImputed = imputed;
+			return mdata;
+		}
+
+		/// <summary>
+		/// Creates an empty default implementation of <see cref="IDocumentData"/>.
+		/// </summary>
+		public static IDocumentData CreateDocumentData() {
+			try {
+				ObjectHandle o = Activator.CreateInstance("DocumentData", "PerseusLibS.Data.Document.DocumentData");
+				return (IDocumentData)o.Unwrap();
+			} catch (Exception) { }
+			return null;
+		}
+		/// <summary>
+		/// Creates an empty default implementation of <see cref="INetworkData"/>.
+		/// </summary>
+		public static INetworkData CreateNetworkData() {
+			try {
+				ObjectHandle o = Activator.CreateInstance("NetworkData", "PerseusLibS.Data.Network.NetworkData");
+				return (INetworkData)o.Unwrap();
+			} catch (Exception) { }
+			return null;
+		}
+		/// <summary>
+		/// Creates an empty default implementation of <see cref="IDataWithAnnotationColumns"/>.
+		/// </summary>
+		public static IDataWithAnnotationColumns CreateDataWithAnnotationColumns() {
+			try {
+				ObjectHandle o = Activator.CreateInstance("DataWithAnnotationColumns", "PerseusLibS.Data.DataWithAnnotationColumns");
+				return (IDataWithAnnotationColumns)o.Unwrap();
+			} catch (Exception) { }
+			return null;
+		}
 	}
 }
