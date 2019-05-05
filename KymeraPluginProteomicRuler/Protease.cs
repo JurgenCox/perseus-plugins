b532ebf1c4b4a1b61e2f4c2efd5ce9d1857e27e9
Daniela Ferretti <ferretti.daniela@outlook.com>
2019-05-05 17:53:50 +0200

    Kymera Proteomic Ruler prep

---
+++ KymeraPluginProteomicRuler/Protease.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace KymeraPluginProteomicRuler{
+	public class Protease{
+		public string name;
+		public Regex cleavageSpecificity;
+
+		public Protease(string name, Regex regex){
+			this.name = name;
+			cleavageSpecificity = regex;
+		}
+	}
+}
