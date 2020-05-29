6b9b72b942070492dd26419d6cf0407a72401962
Juergen Cox <cox@biochem.mpg.de>
2020-05-29 13:51:05 +0200

    Typo

--- PerseusPluginLib/Norm/UnitVector.cs
+++ PerseusPluginLib/Norm/UnitVector.cs
@@ -15,18 +15,17 @@ namespace PerseusPluginLib.Norm{
 		public string[] HelpDocuments => new string[0];
 		public int NumDocuments => 0;
 
-		public string Url
-			=> "http://coxdocs.org/doku.php?id=perseus:user:activities:MatrixProcessing:Normalization:UnitVector";
+		public string Url =>
+			"http://coxdocs.org/doku.php?id=perseus:user:activities:MatrixProcessing:Normalization:UnitVector";
 
 		public string Name => "Unit vectors";
 		public string Heading => "Normalization";
 		public bool IsActive => true;
 		public float DisplayRank => -8;
 
-		public string Description
-			=>
-				"The rows/columns are regarded as high-dimensional vectors. They are divided by their lengts resulting in a matrix of unit vectors."
-			;
+		public string Description =>
+			"The rows/columns are regarded as high-dimensional vectors. They are divided by their lengths " +
+			"resulting in a matrix of unit vectors.";
 
 		public int GetMaxThreads(Parameters parameters){
 			return 1;
@@ -40,13 +39,12 @@ namespace PerseusPluginLib.Norm{
 		}
 
 		public Parameters GetParameters(IMatrixData mdata, ref string errorString){
-			return
-				new Parameters(new Parameter[]{
-					new SingleChoiceParam("Matrix access"){
-						Values = new[]{"Rows", "Columns"},
-						Help = "Specifies if the analysis is performed on the rows or the columns of the matrix."
-					}
-				});
+			return new Parameters(new Parameter[]{
+				new SingleChoiceParam("Matrix access"){
+					Values = new[]{"Rows", "Columns"},
+					Help = "Specifies if the analysis is performed on the rows or the columns of the matrix."
+				}
+			});
 		}
 
 		public void UnitVectors(bool rows, IMatrixData data){
@@ -55,11 +53,11 @@ namespace PerseusPluginLib.Norm{
 					double len = 0;
 					for (int j = 0; j < data.ColumnCount; j++){
 						double q = data.Values.Get(i, j);
-						len += q*q;
+						len += q * q;
 					}
 					len = Math.Sqrt(len);
 					for (int j = 0; j < data.ColumnCount; j++){
-						data.Values.Set(i, j, data.Values.Get(i, j)/ len);
+						data.Values.Set(i, j, data.Values.Get(i, j) / len);
 					}
 				}
 			} else{
@@ -67,11 +65,11 @@ namespace PerseusPluginLib.Norm{
 					double len = 0;
 					for (int i = 0; i < data.RowCount; i++){
 						double q = data.Values.Get(i, j);
-						len += q*q;
+						len += q * q;
 					}
 					len = Math.Sqrt(len);
 					for (int i = 0; i < data.RowCount; i++){
-						data.Values.Set(i, j, data.Values.Get(i, j) /  len);
+						data.Values.Set(i, j, data.Values.Get(i, j) / len);
 					}
 				}
 			}
