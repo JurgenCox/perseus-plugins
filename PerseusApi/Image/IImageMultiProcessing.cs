3d03733e2630512250c235893f7767e33b79483d
Juergen Cox <cox@biochem.mpg.de>
2020-09-29 19:17:03 +0200

    slight changes to image API

--- PerseusApi/Image/IImageMultiProcessing.cs
+++ PerseusApi/Image/IImageMultiProcessing.cs
@@ -1,12 +1,10 @@
 ﻿using BaseLibS.Param;
-using PerseusApi.Document;
 using PerseusApi.Generic;
-using PerseusApi.Matrix;
 
 namespace PerseusApi.Image{
 	public interface IImageMultiProcessing : IImageActivity, IMultiProcessing{
-		IImageData ProcessData(IImageData[] inputData, Parameters param, ref IMatrixData[] supplTables,
-			ref IDocumentData[] documents, ProcessInfo processInfo);
+		IImageData ProcessData(IImageData[] inputData, Parameters param, ref IData[] supplTables,
+			ProcessInfo processInfo);
 
 		Parameters GetParameters(IImageData[] inputData, ref string errString);
 	}
--- PerseusApi/Image/IImageProcessing.cs
+++ PerseusApi/Image/IImageProcessing.cs
@@ -1,12 +1,9 @@
 ﻿using BaseLibS.Param;
-using PerseusApi.Document;
 using PerseusApi.Generic;
-using PerseusApi.Matrix;
 
 namespace PerseusApi.Image{
 	public interface IImageProcessing : IImageActivity, IProcessing{
-		void ProcessData(IImageData mdata, Parameters param, ref IMatrixData[] supplTables,
-			ref IDocumentData[] documents, ProcessInfo processInfo);
+		void ProcessData(IImageData mdata, Parameters param, ref IData[] supplTables, ProcessInfo processInfo);
 
 		Parameters GetParameters(IImageData mdata, ref string errString);
 	}
