ccac78a2638ce06338689ef1542f129c1437452b
Juergen Cox <cox@biochem.mpg.de>
2020-09-30 16:06:33 +0200

    typo

--- PerseusApi/Image/IImageSeries.cs
+++ PerseusApi/Image/IImageSeries.cs
@@ -1,9 +1,9 @@
 ﻿namespace PerseusApi.Image{
 	public interface IImageSeries{
-		int LenthT { get; }
-		int LenthX { get; }
-		int LenthY { get; }
-		int LenthZ { get; }
+		int LengthT { get; }
+		int LengthX { get; }
+		int LengthY { get; }
+		int LengthZ { get; }
 		float GetValueAt(int t, int x, int y, int z);
 	}
 }
