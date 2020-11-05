70b5d580df9503ca5f3896b0a1c0765edf6a878e
Daniela Ferretti <ferretti.daniela@outlook.com>
2020-11-05 13:06:59 +0100

    Conflict INetworkData fixed

---
+++ PerseusApi/Network/INetworkDataAnnColumns.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using PerseusApi.Generic;
+
+namespace PerseusApi.Network
+{
+    /// <summary>
+    /// Network collection table interface
+    /// </summary>
+	public interface INetworkDataAnnColumns : IDataWithAnnotationColumns, IData, IEnumerable<INetworkInfo>
+    {
+#warning This API is experimental and might change frequently
+        /// <summary>
+        /// Get network by index.
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        INetworkInfo this[int i] { get; }
+
+        /// <summary>
+        /// Add a network to the collection.
+        /// </summary>
+        /// <param name="networks"></param>
+        void AddNetworks(params INetworkInfo[] networks);
+    }
+}
