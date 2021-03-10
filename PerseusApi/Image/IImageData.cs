53bb43157b4a46f8010aeb6598a5991619c68eec
bfrohn@biochem.mpg.de <bfrohn@biochem.mpg.de>
2021-03-10 11:15:03 +0100

    fMRI 2.0: Changed data structure

--- PerseusApi/Image/IImageData.cs
+++ PerseusApi/Image/IImageData.cs
@@ -6,13 +6,14 @@ namespace PerseusApi.Image{
 	public interface IImageData : IDataWithAnnotationRows, IData, IEnumerable<IImageSubject>{
 		IImageSubject this[int i]{ get; }
 		void AddSubject(string name, SubjectData subjectData);
-		void AddAnat(float[,,] data, float voxelSizeXmm, float voxelSizeYmm, float voxelSizeZmm);
+		void AddAnat(float[,,] data, float voxelSizeXmm, float voxelSizeYmm, float voxelSizeZmm, 
+			IImageMetadata metadata = null, string name = "");
 
-		void AddFunc(float[,,,] data, float repetitionTimeSeconds, float voxelSizeXmm, float voxelSizeYmm,
-			float voxelSizeZmm, MRIMetadata metadata = null, string name = "");
+		void AddFunc(float[][,,] data, float repetitionTimeSeconds, float voxelSizeXmm, float voxelSizeYmm,
+			float voxelSizeZmm, IImageMetadata metadata = null, string name = "");
 
-		void AddDwi(float[,,,] data, float repetitionTimeSeconds, float voxelSizeXmm, float voxelSizeYmm,
-			float voxelSizeZmm);
+		void AddDwi(float[][,,] data, float repetitionTimeSeconds, float voxelSizeXmm, float voxelSizeYmm,
+			float voxelSizeZmm, IImageMetadata metadata = null, string name = "");
 
 		int Count{ get; }
 	}
---
+++ PerseusApi/Image/SessionData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace PerseusApi.Image{
+	[Serializable]
+	public class SessionData : ICloneable{
+		public object Clone(){
+			return new SubjectData();
+		}
+	}
+}
