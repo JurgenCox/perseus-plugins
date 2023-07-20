be1257dd5675c9e59e49061edb8b375f631377e2
Jurgen <cox@biochem.mpg.de>
2023-07-20 13:28:24 +0200

    reader/writer for Settings

--- PerseusApi/Generic/Settings.cs
+++ PerseusApi/Generic/Settings.cs
@@ -1,5 +1,6 @@
 using System;
-
+using System.IO;
+using BaseLibS.Util;
 namespace PerseusApi.Generic{
 	[Serializable]
 	public class Settings{
@@ -10,6 +11,17 @@ namespace PerseusApi.Generic{
 		public Settings(){
 			Nthreads = Environment.ProcessorCount;
 			CommentPrefix = new[]{"#", "!"};
+			TempFolder = "";
+		}
+		public Settings(BinaryReader reader){
+			Nthreads = reader.ReadInt32();
+			CommentPrefix = FileUtils.ReadStringArray(reader);
+			TempFolder = reader.ReadString();
+		}
+		public void Write(BinaryWriter writer){
+			writer.Write(Nthreads);
+			FileUtils.Write(CommentPrefix, writer);
+			writer.Write(TempFolder);
 		}
 	}
 }
